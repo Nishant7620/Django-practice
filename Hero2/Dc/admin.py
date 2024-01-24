a94761e54f647ac6599b720adef894733b3018e7
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-24 20:08:51 +0530

    updated

--- Hero2/Dc/admin.py
+++ Hero2/Dc/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Dc.models import batman
 # Register your models here.
+@admin.register(batman)
+class DcAdmmin(admin.ModelAdmin):
+    list_display = ['id','name','heroic_name']
+
+# admin.site.register(batman)
--- Hero2/Dc/models.py
+++ Hero2/Dc/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class batman(models.Model):
+    name = models.CharField(max_length = 50)
+    heroic_name = models.CharField(max_length = 50)
